bd8e5d9d15fb9196205e8073189bcba84d8f9876
solivera2 <53319338+solivera2@users.noreply.github.com>
2024-06-04 14:33:46 -0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+# % weight=100 color=#0fbc11 icon="\uf1ec"
+@namespace
+class sumadosnumeros:
+    # % block
+    def sumando(num1: number, num2: number):
+        resultado = num1 + num2
+        return resultado
